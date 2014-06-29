499f8c9ee70f1982bce8ffa6c847d200289a5643
Dirk <me@dirkz.com>
2014-06-29 09:50:54 +0200

    preparations for registering

--- src/hue_control/core.cljs
+++ src/hue_control/core.cljs
@@ -7,7 +7,20 @@
            [goog Uri]))
 
 (defrecord Bridge [ip id mac name])
-(defrecord User [name password])
+(defrecord User [devicetype username])
+
+(defn platform-id []
+  (let [nav js/navigator]
+    (.replace (str (.-platform nav)) " " "_")))
+
+(defn make-user []
+  (User. "hue-control" (platform-id)))
+
+(defn api-url [bridge]
+  (str "http://" (:ip bridge) "/api/"))
+
+(defn register-url [bridge user]
+  (str (api-url bridge) (:devicetype user)))
 
 (defn bridge-from-json [xs]
   (.log js/console xs)
@@ -27,13 +40,25 @@
                      (put! out (bridge-from-json obj)))))
     out))
 
+(defn post-json [uri data]
+  (let [out (chan)]
+    (.send XhrIo uri
+           (fn [e] (let [xhr (.-target e)
+                         obj (.getResponseJson xhr)]
+                     (put! out (bridge-from-json obj))))
+           "POST" data)))
+
 (defn load-bridges-url []
   "https://www.meethue.com/api/nupnp")
 
 (defn load-bridges []
   (load-json (load-bridges-url)))
 
+(defn register-bridge [bridge user]
+  (post-json (register-url user bridge) (clj->js user)))
+
 (defn init []
+  (.log js/console (make-user))
   (go
     (let [bridges (<! (load-bridges))]
       (doseq [b bridges]
